9f6726d3bececf79248a9a5b6edf2ad32dfd9e45
onkar <ophulwalkar77@gmail.com>
2025-01-22 00:04:38 +0530

    Done: StartHere

---
+++ StartHere/math.js
@@ -0,0 +1,14 @@
+// //-----To export at once
+// const add = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const divide = (a, b) => a / b;
+
+// module.exports = {add, subtract, multiply, divide};
+
+
+// //------To export one by one
+// exports.add = (a, b) => a + b;
+// exports.subtract = (a, b) => a - b;
+// exports.multiply = (a, b) => a * b;
+// exports.divide = (a, b) => a / b;
